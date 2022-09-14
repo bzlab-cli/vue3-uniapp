baa6f99affd474d53249f9b2e871d9e14ef71356
zhangbinjie <1435255141@qq.com>
2022-09-14 17:45:39 +0800

    update

---
+++ bin/publish.js
@@ -0,0 +1,85 @@
+const ci = require('miniprogram-ci')
+const fs = require('fs')
+const projectConfig = require('../project.config.json')
+const dayjs = require('dayjs')
+const argvs = JSON.parse(process.env.npm_config_argv).original
+
+function getEnv() {
+  let env = 'develop'
+  argvs.some(argv => {
+    if (argv.includes('--mode')) {
+      const result = argv.split('=')
+      if (result) {
+        const [, envTag] = result
+        env = envTag
+        return true
+      }
+    }
+    return false
+  })
+  return env
+}
+
+function getBranch() {
+  let env = getEnv()
+  const obj = {
+    deploy: 'release',
+    prod: 'release'
+  }
+  return obj[env]
+}
+
+function getRobot() {
+  let env = getEnv()
+  const obj = {
+    deploy: 1,
+    prod: 2
+  }
+  return obj[env]
+}
+
+function getRobotName() {
+  let env = getEnv()
+  const obj = {
+    deploy: '测试机器人',
+    prod: '生产机器人'
+  }
+  return obj[env]
+}
+
+async function upload() {
+  let pkg = fs.readFileSync('./package.json').toString()
+  let curVersion = JSON.parse(pkg).version
+  let random = dayjs().format('HHmmss')
+  const [major, minor] = curVersion.split('.')
+
+  const version = `${major}.${minor}.${random}`
+  const versionDesc = `类型：${getRobotName()}\r\n版本：${version}\r\n分支: ${getBranch()}`
+  console.log('versionDesc', versionDesc)
+
+  const project = new ci.Project({
+    appid: projectConfig.appid,
+    type: 'miniProgram',
+    projectPath: './dist/build/mp-weixin/',
+    privateKeyPath: './ci-private.key',
+    ignores: ['node_modules/**/*']
+  })
+
+  try {
+    await ci.upload({
+      project,
+      version,
+      desc: versionDesc,
+      robot: getRobot(),
+      setting: {
+        es7: true,
+        minify: true,
+        autoPrefixWXSS: true
+      }
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+upload()
---
+++ bin/upload.js
@@ -0,0 +1,1 @@
+throw new Error('[错误]禁止在开发者工具上传，请前往jenkins部署')
